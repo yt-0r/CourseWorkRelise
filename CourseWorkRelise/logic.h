ece048a84d3be3aac2ffc0a6a5a82c37c057f3c3
Никита Мартов <cool.nikitasj@yandex.ru>
2023-05-21 18:25:27 +0300

    create logic.h

--- CourseWorkRelise/logic.h
+++ CourseWorkRelise/logic.h
@@ -1,1 +1,11 @@
 #pragma once
+#pragma warning(disable:6385)
+#pragma warning(disable:6386)
+#pragma warning(disable:4244)
+#include <cmath>
+#include <ctime>
+#include "PublicVar.h"
+
+int AroundCell(int i, int j, int b[size + 2][size + 2]);
+int Ship_placement(int b[size + 2][size + 2], int size);
+
