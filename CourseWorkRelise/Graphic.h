6cfd51c3e81d68ee5b18c57051437220357f8c2e
Захар Кистин <kistin2003@mail.ru>
2023-05-21 22:54:39 +0700

    DrawGrid func was added

--- CourseWorkRelise/Graphic.h
+++ CourseWorkRelise/Graphic.h
@@ -4,6 +4,6 @@
 
 using namespace sf;
 
-//int drawGrid(RenderWindow& window, int Gridp[size + 2][size + 2], int Gridc[size + 2][size + 2], bool isPlayerGrid);
+int drawGrid(RenderWindow& window, int Gridp[size + 2][size + 2], int Gridc[size + 2][size + 2], bool isPlayerGrid);
 
 int initGraphic();
