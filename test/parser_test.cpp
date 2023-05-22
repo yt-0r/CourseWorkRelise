c775d3455adc4e386daf94f665444c5b9c7e8449
Захар Кистин <kistin2003@mail.ru>
2023-05-23 04:38:22 +0700

    newtest

--- test/parser_test.cpp
+++ test/parser_test.cpp
@@ -1,14 +1,24 @@
 #include "../thirdparty/ctest.h"
 
 #include <Audio.h>
+#include <logic.h>
 
-CTEST (ctest, loadTextureFromFile) {
+CTEST (ctest, loadAudioFromFile) {
     SoundBuffer soundTrue;
-    bool checkAudioTrue = loadTextureFromFile(screenTrue, "resources/sfx/Background/shoot.wav") ;
+    bool checkAudioTrue = loadTextureFromFile(soundTrue, "resources/sfx/Background/shoot.wav") ;
     ASSERT_TRUE(checkAudioTrue);
     
 
-    SoundBuffer soundTrue;
-    bool checkAudioFalse = loadTextureFromFile(screenFalse, "resources/sfx/Background/shoot.wav") ;
+    SoundBuffer soundFalse;
+    bool checkAudioFalse = loadTextureFromFile(soundFalse, "resources/sfx/Background/shooting.wav") ;
     ASSERT_FALSE(checkAudioFalse);
+}
+
+CTEST (ctest, zeroGridCheck) {
+    bool checkzeroGridTrue = zeroGrid(int Gridp[size + 2][size + 2], int Gridc[size + 2][size + 2]) ;
+    ASSERT_TRUE(checkzeroGridTrue);
+    
+
+    bool checkzeroGridFalse = zeroGrid(int Gridp[size + 2][size + 4], int Gridc[size + 3][size + 2]) ;
+    ASSERT_FALSE(checkzeroGridFalse);
 }
